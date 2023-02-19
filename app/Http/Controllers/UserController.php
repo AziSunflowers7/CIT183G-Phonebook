c770d5893ecbcb48a84fd90429ca10e7d14e8651
AziSunflowers7 <azisunflowers7@gmail.com>
2023-02-19 17:21:37 +0800

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+   public function index(){
+    return 'Hello from user';
+}
+public function show($id){
+    $data=array(
+    "id"=>$id,
+    "name"=>"LeonardReyes",
+    "age"=>30,
+    "email"=>"primus.reyes@gmail.com"
+     );
+     return view('user', $data);
+}
+}
